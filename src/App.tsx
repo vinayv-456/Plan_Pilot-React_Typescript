b496c4a6cdf33cd88b68bce747aa04231775976e
vinayv-456 <vinayvallabineni456@gmail.com>
2024-02-26 01:55:40 -0500

    add inputfeild to enter todo item
    -- added typescript for various parts of the components like props, state, ref etc...

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Todo } from "./models/model";
+import InputField from "./components/InputField/InputField";
 
-function App() {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  console.log("todos", todos);
+
+  const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodos((prev) => [...prev, { id: 1, name: todo, isDone: true }]);
+    setTodo("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col bg-blue-500 h-screen w-screen">
+      <span className="mx-auto my-3 text-3xl font-medium text-slate-50">
+        Plan Pilot
+      </span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      {todos.map((e) => (
+        <span>{e.name}</span>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (input: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    inputRef.current?.blur();
+    handleAdd(e);
+  };
+
+  return (
+    <form className="w-full" onSubmit={(e) => handleSubmit}>
+      <input
+        ref={inputRef}
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
+};
+
+export default InputField;
